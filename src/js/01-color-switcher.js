1c067de2f1870512ed157c6071d9c72228b88473
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-14 17:44:03 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+buttonStop.disabled = true;
+
+function startColorsChange() {
+    bodyEl.style.backgroundColor = getRandomHexColor(); 
+     buttonStart.disabled = true;
+    buttonStop.disabled = false;
+};
+
+const onButtonStart = () => {
+    timerId = setInterval(startColorsChange, 1000);
+};
+
+const onButtonStop = () => {
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+buttonStart.addEventListener('click', startColorsChange);
+buttonStart.addEventListener('click', onButtonStart);
+buttonStop.addEventListener('click', onButtonStop);
