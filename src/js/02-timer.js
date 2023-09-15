ff633d2f7b726038c347732245351fbdb266d659
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-09-15 19:27:44 +0300

    tsk 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,12 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const buttonStart = document.querySelector('button[data-start]');
 const inputEl = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
+
+buttonStart.disabled = true;
 
  const options = {
   enableTime: true,
@@ -12,10 +18,11 @@ const inputEl = document.querySelector('#datetime-picker');
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     const selectedDate = selectedDates[0].getTime();
-    const CurrentDate = Date.nov();
+   const currentDate = Date.now();
 
-    if (selectedDate <= CurrentDate) {
+    if (selectedDate <= currentDate) {
       alert('Please choose a date in the future');
+      inputEl._flatpickr.setDate(new Date());
     } else {
 buttonStart.disabled = false;
     }
@@ -24,10 +31,41 @@ buttonStart.disabled = false;
 
 flatpickr("#datetime-picker", options);
 
- 
+function onButtonStart() {
+const celectedDate = new Date(inputEl.value).getTime();
+  const currentDate = Date.now();
+  const dateDifference = celectedDate - currentDate;
 
-function onSelectDate(event) {
-   
-};
+  const timeCounter = convertMs(dateDifference);
+  console.log(timeCounter);
+  setInterval(() => {
+    daysEl.textContent = timeCounter.days;
+    hoursEl.textContent = timeCounter.hours;
+    minutesEl.textContent = timeCounter.minutes;
+    secondsEl.textContent = timeCounter.seconds;
+  }, 1000);
 
-inputEl.addEventListener('input', onSelectDate)
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+buttonStart.addEventListener('click', onButtonStart);
